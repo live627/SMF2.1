bf3b68cb4e7c355f0ce315eceb7402f269354346
orstio <orstio@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2006-09-04 23:02:34 +0000

    + Add logout functionality to Mambo 4.6 login bot. (Mambo 4.6 bridge/SMF_login.php)
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@5053 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- other/bridges/mambo_4.6/SMF_login.php
+++ other/bridges/mambo_4.6/SMF_login.php
@@ -5,7 +5,7 @@
 * SMF: Simple Machines Forum                                                  *
 * Open-Source Project Inspired by Zef Hemel (zef@zefhemel.com)                *
 * =========================================================================== *
-* Software Version:           SMF 1.1 RC2                                     *
+* Software Version:           SMF 2.0                                    *
 * Software by:                Simple Machines (http://www.simplemachines.org) *
 * Copyright 2001-2006 by:     Lewis Media (http://www.lewismedia.com)         *
 * Support, News, Updates at:  http://www.simplemachines.org                   *
@@ -25,7 +25,7 @@
 if (!defined('_VALID_MOS'))
 	die('Direct Access to this location is not allowed.');
 
-global $db_name, $_MAMBOTS;
+global $db_name, $database;
 
 	$database =& mamboDatabase::getInstance();
 	$mainframe =& mosMainFrame::getInstance();
@@ -33,13 +33,14 @@ global $db_name, $_MAMBOTS;
 class SMF_Login {
 
 	function register () {
-		return ('goodLogin');
+		return 'goodLogin';
 	}
 
 	function perform( $loginfo ) {
 
 		global $db_name, $db_prefix, $mosConfig_db, $mosConfig_dbprefix, $mosConfig_live_site;
-	
+
+
 		//Start up the integration
 		$database =& mamboDatabase::getInstance();
 		$mainframe =& mosMainFrame::getInstance();
@@ -61,67 +62,87 @@ class SMF_Login {
 		foreach ($loginfo as $k => $v){
 				$$k = $v;
 			}
-
-		mysql_select_db($db_name);
-		//check to make sure this user even exists in SMF
-		$get_password_hash = mysql_query ("
-		SELECT passwd, ID_MEMBER, ID_GROUP, lngfile, is_activated, emailAddress, additionalGroups, memberName, passwordSalt
-		FROM {$db_prefix}members
-		WHERE memberName = '$_user'
-		LIMIT 1");
-		$user_settings = mysql_fetch_assoc($get_password_hash);
-
-		$hash_input_password = sha1(strtolower($user_settings['memberName']) . un_htmlspecialchars(stripslashes($_password)));
-
-		
-		if ($user_settings['passwd']==$hash_input_password)
-		{				
-			if ($_remember)
-				$cookielength = 3153600;
-			else 
-				$cookielength = 3600;
-		
-			require_once($smf_path.'/Sources/Subs-Auth.php');
-			setLoginCookie( $cookielength, $user_settings['ID_MEMBER'], sha1($user_settings['passwd'] . $user_settings['passwordSalt']));
-
+		require_once($smf_path.'/Sources/Subs-Auth.php');			
+		//Are we logging out?
+		if ($_REQUEST['option']=='logout'){
+			mysql_select_db($db_name);
+			
+			mysql_query("			
+				DELETE FROM {$db_prefix}log_online
+				WHERE ID_MEMBER = $ID_MEMBER
+				LIMIT 1");
+					
+			// Empty the cookie! (set it in the past, and for ID_MEMBER = 0)
+			setLoginCookie(-3600, 0);
 			mysql_select_db($mosConfig_db);
-			if ($session === null) $session =& mosSession::getCurrent();
-			$database =& mamboDatabase::getInstance();
-			$database->setQuery( "SELECT id, gid, block, usertype"
-				. "\nFROM #__users"
-				. "\nWHERE username='$user_settings[memberName]' AND password='".md5($_password)."'"
-			);
-			if ($database->loadObject($row)) {
-				if ($row->block) {
-					$message = T_('Your login has been blocked. Please contact the administrator.');
-					return false;
+
+			setcookie("usercookie[username]", "", 0, "/");
+			setcookie("usercookie[password]", "", 0, "/");
+		
+			return true;
+		//If we're not logging out, then we're logging in	
+		} else {
+		
+			mysql_select_db($db_name);
+			//check to make sure this user even exists in SMF
+			$get_password_hash = mysql_query ("
+				SELECT passwd, ID_MEMBER, ID_GROUP, lngfile, is_activated, emailAddress, additionalGroups, memberName, passwordSalt
+				FROM {$db_prefix}members
+				WHERE memberName = '$_user'
+				LIMIT 1");
+			$user_settings = mysql_fetch_assoc($get_password_hash);
+
+			$hash_input_password = sha1(strtolower($user_settings['memberName']) . un_htmlspecialchars(stripslashes($_password)));
+
+			if ($user_settings['passwd']==$hash_input_password)
+			{				
+				if ($_remember)
+					$cookielength = 3153600;
+				else 
+					$cookielength = 3600;
+		
+
+				setLoginCookie( $cookielength, $user_settings['ID_MEMBER'], sha1($user_settings['passwd'] . $user_settings['passwordSalt']));
+
+				mysql_select_db($mosConfig_db);
+				if ($session === null) $session =& mosSession::getCurrent();
+				$database =& mamboDatabase::getInstance();
+				$database->setQuery( "SELECT id, gid, block, usertype"
+					. "\nFROM #__users"
+					. "\nWHERE username='$user_settings[memberName]' AND password='".md5($_password)."'"
+				);
+				if ($database->loadObject($row)) {
+					if ($row->block) {
+						$message = T_('Your login has been blocked. Please contact the administrator.');
+						return false;
+					}
+				// fudge the group stuff
+				//			$grp = $acl->getAroGroup( $row->id );
+				//			if ($acl->is_group_child_of( $grp->name, 'Registered', 'ARO' ) ||
+				//			$acl->is_group_child_of( $grp->name, 'Public Backend', 'ARO' )) {
+				// fudge Authors, Editors, Publishers and Super Administrators into the Special Group
+				//			$row->usertype = $grp->name;
+					$session->guest = 0;
+					$session->username = $username;
+					$session->userid = $row->id;
+					$session->usertype = $row->usertype;
+					if ($row->usertype == 'Registered') $session->gid = 1;
+					else $session->gid = 2;
+					$session->gid = intval( $row->gid ); # what is going on here???
+					$session->update();
+					$currentDate = date("Y-m-d\TH:i:s");
+					$query = "UPDATE #__users SET lastvisitDate='$currentDate' where id='$session->userid'";
+					$database->setQuery($query);
+					if (!$database->query()) {
+						die($database->stderr(true));
+					}
+					setcookie("usercookie[username]", $user_settings['memberName'], $cookielength, "/");
+					setcookie("usercookie[password]", md5($_password), $cookielength, "/");
 				}
-			// fudge the group stuff
-//			$grp = $acl->getAroGroup( $row->id );
-//			if ($acl->is_group_child_of( $grp->name, 'Registered', 'ARO' ) ||
-//			$acl->is_group_child_of( $grp->name, 'Public Backend', 'ARO' )) {
-			// fudge Authors, Editors, Publishers and Super Administrators into the Special Group
-//			$row->usertype = $grp->name;
-				$session->guest = 0;
-				$session->username = $username;
-				$session->userid = $row->id;
-				$session->usertype = $row->usertype;
-				if ($row->usertype == 'Registered') $session->gid = 1;
-				else $session->gid = 2;
-				$session->gid = intval( $row->gid ); # what is going on here???
-				$session->update();
-				$currentDate = date("Y-m-d\TH:i:s");
-				$query = "UPDATE #__users SET lastvisitDate='$currentDate' where id='$session->userid'";
-				$database->setQuery($query);
-				if (!$database->query()) {
-					die($database->stderr(true));
-				}
-				setcookie("usercookie[username]", $user_settings['memberName'], $cookielength, "/");
-				setcookie("usercookie[password]", md5($_password), $cookielength, "/");
 			}
+			mysql_select_db($mosConfig_db);
+			return true;
 		}
-		mysql_select_db($mosConfig_db);
-		return true;
 	}
 }
 
