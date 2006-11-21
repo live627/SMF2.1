9a50ffc70c3783d6ac1f9d0255c4862ee0c0fd70
akabugeyes <akabugeyes@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2006-11-21 18:39:00 +0000

    excempt => exempt
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@5321 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/languages/ModSettings.english.php
+++ Themes/default/languages/ModSettings.english.php
@@ -48,7 +48,7 @@ $txt['max_image_height'] = 'Max height of posted pictures (0 = disable)';
 $txt['enableReportPM'] = 'Enable reporting of personal messages';
 $txt['max_pm_recipients'] = 'Maximum number of recipients allowed in a personal message.<div class="smalltext">(0 for no limit, admins are exempt)</div>';
 $txt['pm_posts_verification'] = 'Post count under which users must enter code when sending personal messages.<div class="smalltext">(0 for no limit, admins are exempt)</div>';
-$txt['pm_posts_per_hour'] = 'Number of personal messages a user may send in an hour.<div class="smalltext">(0 for no limit, moderators are excempt)</div>';
+$txt['pm_posts_per_hour'] = 'Number of personal messages a user may send in an hour.<div class="smalltext">(0 for no limit, moderators are exempt)</div>';
 
 $txt['mods_cat_layout'] = 'Layout and Options';
 $txt['compactTopicPagesEnable'] = 'Limit number of displayed page links';
