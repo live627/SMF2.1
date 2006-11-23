182b7f81d0fa24f602d55001d48d29d29b2e73b2
akabugeyes <akabugeyes@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2006-11-23 05:22:18 +0000

    Changed two misleading strings.
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@5325 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/languages/Profile.english.php
+++ Themes/default/languages/Profile.english.php
@@ -261,8 +261,8 @@ $txt['rtm8'] = 'Poster';
 
 $txt[732] = 'Show board descriptions inside boards.';
 $txt['show_children'] = 'Show child boards on every page inside boards, not just the first.';
-$txt['show_no_avatars'] = 'Don\'t show other users\' avatars.';
-$txt['show_no_signatures'] = 'Don\'t show other users\' signatures.';
+$txt['show_no_avatars'] = 'Don\'t show users\' avatars.';
+$txt['show_no_signatures'] = 'Don\'t show users\' signatures.';
 $txt['show_no_censored'] = 'Leave words uncensored.';
 $txt['calendar_start_day'] = 'First day of the week on the calendar';
 $txt['display_quick_reply'] = 'Use quick reply on topic display: ';
