2f6b24ea5e012703e146b721d5a849fc13f6823b
orstio <orstio@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2006-12-29 14:46:27 +0000

    - Removed Joomla 1.5 bridge to avoid confusion. (Several files)
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@5487 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- other/bridges/joomla_1.5/config.smf.php
+++
@@ -1,6 +0,0 @@
-<?php
-global $smf_path,$wrapped, $bridge_reg;
-$smf_path = "";
-$wrapped = true;
-$bridge_reg = true;
-?>
--- other/bridges/joomla_1.5/config.smf_registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-global $agreement_required, $im, $pm_on_reg, $use_realname;
-$agreement_required = "on";
-$im = "on";
-$pm_on_reg = "on";
-$use_realname = false;
-?>
