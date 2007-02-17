bc12eb9afa36ba6f8f90663f7b5f31180fae6b28
mikemill <mikemill@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2007-02-17 13:55:16 +0000

    Remove the Dutch, German, and Spanish language files from this CVS module.
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@5640 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/languages/Modifications.german.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Version: 2.0 Alpha; Modifications
-
-// Important! Before editing these language files please read the text at the topic of index.german.php.
-
-?>
--- Themes/default/languages/Settings.dutch.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Version: 2.0 Alpha; Settings
-
-// Important! Before editing these language files please read the text at the topic of index.dutch.php.
-
-$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
-$txt['theme_description'] = 'Het standaardthema van Simple Machines.<br /><br />Met dank aan Bloc en het designteam.';
-
-?>
