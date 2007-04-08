04a56e6f2039e418bf928adb5bf7d97504601deb
mikemill <mikemill@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2007-04-08 16:18:16 +0000

    Wrong text index.
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@5746 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/Register.template.php
+++ Themes/default/Register.template.php
@@ -72,7 +72,7 @@ function template_before()
 						</td>
 						<td>
 							<input type="text" name="email" id="smf_autov_reserve1" size="30" tabindex="', $context['tabindex']++, '" />
-							<label for="hide_email"><input type="checkbox" name="hide_email" id="hide_email" class="check" /> ', $txt['hide_email'], '</label>
+							<label for="hide_email"><input type="checkbox" name="hide_email" id="hide_email" class="check" /> ', $txt['allow_user_email'], '</label>
 						</td>
 					</tr>
 					<tr>
