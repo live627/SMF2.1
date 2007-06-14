eb71564fb333643c6970caed9336551397caa0c6
daniel <daniel@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2007-06-14 17:59:43 +0000

    *** empty log message ***
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@5850 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/classic/languages/Settings.dutch.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Version: 2.0 Alpha; Settings
-
-// Important! Before editing these language files please read the text at the topic of index.dutch.php.
-
-$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
-$txt['theme_description'] = 'Het klassieke uiterlijk, dat gebruikt werd voor de revolutionaire PHP/MySQL overzetting van YaBB en dat onderscheidend is geweest tot de hergeboorte als SMF.<br /><br />Auteur: <i><a href="mailto:webmaster@yabbse.org">het YaBB SE Team</a></i>.';
-
-?>
