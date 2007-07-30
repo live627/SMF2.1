3c956de6873d7ceed7bfbebf35fee6685bb55dd8
grudge <grudge@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2007-07-30 08:28:59 +0000

    *** empty log message ***
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@5908 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/languages/index.english.php
+++ Themes/default/languages/index.english.php
@@ -22,8 +22,9 @@ $txt['lang_locale'] = 'en_US';
 $txt['lang_dictionary'] = 'en';
 $txt['lang_spelling'] = 'american';
 
-// Character set and right to left?
+// Ensure you remember to use uppercase for character set strings.
 $txt['lang_character_set'] = 'ISO-8859-1';
+// Character set and right to left?
 $txt['lang_rtl'] = false;
 
 $txt['days'] = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
