8bb790762156d9ddb3315d2ddb9abd97e9fad97c
grudge <grudge@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2008-01-01 17:00:26 +0000

    ! Another year goes by...
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@6411 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/languages/index.english.php
+++ Themes/default/languages/index.english.php
@@ -438,7 +438,7 @@ $txt['go_up'] = 'Go Up';
 $txt['go_down'] = 'Go Down';
 
 $forum_copyright = '<a href="http://www.simplemachines.org/" title="Simple Machines Forum" rel="new_win">Powered by %s</a> |
- <a href="http://www.simplemachines.org/about/copyright.php" title="Free Forum Software" rel="new_win">SMF &copy; 2006&ndash;2007, Simple Machines LLC</a>';
+ <a href="http://www.simplemachines.org/about/copyright.php" title="Free Forum Software" rel="new_win">SMF &copy; 2006&ndash;2008, Simple Machines LLC</a>';
 
 $txt['birthdays'] = 'Birthdays:';
 $txt['events'] = 'Events:';
