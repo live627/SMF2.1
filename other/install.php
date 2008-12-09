fe4fe3477392be8a6e4e140eb5a8f9613664514a
compuart <compuart@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2008-12-09 21:19:21 +0000

    ! Attempt an alternative way of retrieving the version as the pg_version function doesn't exist yet in PHP 4.x. (install.php) [Bug 2790]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@7677 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- other/install.php
+++ other/install.php
@@ -54,7 +54,7 @@ $databases = array(
 		'name' => 'PostgreSQL',
 		'version' => '7.4.10',
 		'function_check' => 'pg_connect',
-		'version_check' => '$version = pg_version(); return $version[\'client\'];',
+		'version_check' => '$request = pg_query(\'SELECT version()\'); list ($version) = pg_fetch_row($request); return $version;',
 		'supported' => function_exists('pg_connect'),
 		'always_has_db' => true,
 		'validate_prefix' => create_function('&$value', '
