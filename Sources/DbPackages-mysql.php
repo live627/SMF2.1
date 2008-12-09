0165b5e423613d6a8f08ab7cd987475536fdd273
compuart <compuart@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2008-12-09 20:56:53 +0000

    ! The size property wasn't properly handled by the create table functions in the database layer (DbPackages-mysql.php, DbPackages-postgresql.php, DbPackages-sqlite.php) [Bug 2761]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@7676 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Sources/DbPackages-mysql.php
+++ Sources/DbPackages-mysql.php
@@ -173,7 +173,7 @@ function smf_db_create_table($table_name, $columns, $indexes = array(), $paramet
 			$default = '';
 
 		// Sort out the size... and stuff...
-		$column['size'] = isset($column['size']) && is_numeric($column_info['size']) ? $column['size'] : null;
+		$column['size'] = isset($column['size']) && is_numeric($column['size']) ? $column['size'] : null;
 		list ($type, $size) = $smcFunc['db_calculate_type']($column['type'], $column['size']);
 		if ($size !== null)
 			$type = $type . '(' . $size . ')';
