5ea98b6f1ee5384ddb145188e6f45f4f5618c5d2
compuart <compuart@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2008-12-14 20:46:47 +0000

    ! The version_compatible property of the standard search API was incorrect. (SearchAPI-Standard.php) [Bug 2776]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@7707 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Sources/SearchAPI-Standard.php
+++ Sources/SearchAPI-Standard.php
@@ -28,7 +28,7 @@ if (!defined('SMF'))
 class standard_search
 {
 	// This is the last version of SMF that this was tested on, to protect against API changes.
-	public $version_compatible = 'SMF 2.0 Beta RC1';
+	public $version_compatible = 'SMF 2.0 RC1';
 
 	// This won't work with versions of SMF less than this.
 	public $min_smf_version = 'SMF 2.0 Beta 2';
