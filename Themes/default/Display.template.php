268d5fe173a7e0fbc4485fcb7171533e65f9b2e9
aaronvg <aaronvg@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2009-01-13 21:08:40 +0000

    ! The container of the inline message icon changer wasn't getting the proper width in all browsers. (script.js) [Bug 2176]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@7826 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/Display.template.php
+++ Themes/default/Display.template.php
@@ -385,8 +385,9 @@ function template_main()
 		// Show the post itself, finally!
 		echo '
 					</ul>';
+
 		if ($ignoring)
-			echo '<div id="msg_', $message['id'], '_ignored_prompt" style="display: none;">', $txt['ignoring_user'], '  <a href="#msg', $message['id'], '" onclick="return ignoreToggles[', $message['id'], '].toggle()">', $txt['show_ignore_user_post'], '</a></div>';
+			echo '<div id="msg_', $message['id'], '_ignored_prompt" style="display: none; clear: left">', $txt['ignoring_user'], '  <a href="#msg', $message['id'], '" onclick="return ignoreToggles[', $message['id'], '].toggle()">', $txt['show_ignore_user_post'], '</a></div>';
 
 		echo '
 					<div class="post" id="msg_', $message['id'], '"', '>';
