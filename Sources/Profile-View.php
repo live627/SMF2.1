000dba2237fc4939268df4c4241e5a368775bfbe
regularexpression <regularexpression@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2009-03-11 01:14:29 +0000

    ! Warning system, make sure things which are disabled stay disabled. (Profile-View.php) [Bug 3104]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@8022 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Sources/Profile-View.php
+++ Sources/Profile-View.php
@@ -1808,6 +1808,11 @@ function viewWarning($memID)
 	if (!allowedTo('issue_warning') && (empty($modSettings['warning_show']) || ($modSettings['warning_show'] == 1 && !$context['user']['is_owner'])))
 		fatal_lang_error('no_access');
 
+	// Make sure things which are disabled stay disabled.
+	$modSettings['warning_watch'] = !empty($modSettings['warning_watch']) ? $modSettings['warning_watch'] : 110;
+	$modSettings['warning_moderate'] = !empty($modSettings['warning_moderate']) ? $modSettings['warning_moderate'] : 110;
+	$modSettings['warning_mute'] = !empty($modSettings['warning_mute']) ? $modSettings['warning_mute'] : 110;
+	
 	// Let's use a generic list to get all the current warnings, and use the issue warnings grab-a-granny thing.
 	require_once($sourcedir . '/Subs-List.php');
 	require_once($sourcedir . '/Profile-Actions.php');
