eed1a499dc2555d4aa8cc3706a4ac8f4891932de
regularexpression <regularexpression@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2009-04-06 16:36:31 +0000

    ! PostGresql requires all non-aggregate fields in the select must be referenced in the group by (Subs-Post.php) [Bug 3193]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@8222 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Sources/Subs-Post.php
+++ Sources/Subs-Post.php
@@ -2793,7 +2793,7 @@ function sendApprovalNotifications(&$topicData)
 			AND mem.is_activated = {int:is_activated}
 			AND mem.notify_types < {int:notify_types}
 			AND mem.notify_regularity < {int:notify_regularity}
-		GROUP BY mem.id_member, ln.id_topic
+		GROUP BY mem.id_member, ln.id_topic, mem.email_address, mem.notify_regularity, mem.notify_types, mem.notify_send_body, mem.lngfile, ln.sent, mem.id_group, mem.additional_groups, b.member_groups, mem.id_post_group, t.id_member_started
 		ORDER BY mem.lngfile',
 		array(
 			'topic_list' => $topics,
