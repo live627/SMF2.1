4d2935d23a33f4883e258e28ca57ca09b36f721a
regularexpression <regularexpression@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2009-04-23 10:14:39 +0000

    Reverting Revision 8130 - replying to own messages in conversation (PersonalMessage.template.php)
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@8281 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/PersonalMessage.template.php
+++ Themes/default/PersonalMessage.template.php
@@ -289,13 +289,9 @@ function template_folder()
 						echo '
 										<a href="', $scripturl, '?action=pm;sa=send;f=', $context['folder'], $context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '', ';pmsg=', $message['id'], ';quote;u=all">', $reply_all_button, '</a>', $context['menu_separator'];
 
-					// No replying/quoting to yourself.
-					if($message['member']['id'] != $context['user']['id'])
-					{
-						echo '
+					echo '
 										<a href="', $scripturl, '?action=pm;sa=send;f=', $context['folder'], $context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '', ';pmsg=', $message['id'], ';quote', $context['folder'] == 'sent' ? '' : ';u=' . $message['member']['id'], '">', $quote_button, '</a>', $context['menu_separator'], '
 										<a href="', $scripturl, '?action=pm;sa=send;f=', $context['folder'], $context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '', ';pmsg=', $message['id'], ';u=', $message['member']['id'], '">', $reply_button, '</a> ', $context['menu_separator'];
-					}
 				}
 				// This is for "forwarding" - even if the member is gone.
 				else
