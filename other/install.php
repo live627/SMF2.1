b6a82c7abdc60357e937d57674bac816e76cd8a4
sleepy <sleepy@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2009-05-03 02:23:12 +0000

    ! Database creation should use UTF-8 as default character set if we are using UTF-8 (install.php) [Bug 997]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@8329 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- other/install.php
+++ other/install.php
@@ -806,7 +806,7 @@ function DatabaseSettings()
 		if ($db_name != '' && !$needsDB)
 		{
 			$smcFunc['db_query']('', "
-				CREATE DATABASE IF NOT EXISTS `$db_name`",
+				CREATE DATABASE IF NOT EXISTS `$db_name`" . (isset($_POST['utf8']) ? ' DEFAULT CHARACTER SET utf8' : ''),
 				array(
 					'security_override' => true,
 					'db_error_skip' => true,
