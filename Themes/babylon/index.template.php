25a0e3f9c0051009ba7e99f77f4f74c955e8bc4b
regularexpression <regularexpression@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2009-06-06 15:52:33 +0000

    * Use add_load_event instead of window.onload for pm popup (index.template.php) [Bug 3490]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@8586 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/babylon/index.template.php
+++ Themes/babylon/index.template.php
@@ -85,10 +85,12 @@ function template_html_above()
 		var smf_scripturl = "', $scripturl, '";
 		var smf_iso_case_folding = ', $context['server']['iso_case_folding'] ? 'true' : 'false', ';
 		var smf_charset = "', $context['character_set'], '";', $context['show_pm_popup'] ? '
-		window.onload = function() {
+		var func = function ()
+		{
 			if (confirm("' . $txt['show_personal_messages'] . '"))
 				window.open(smf_prepareScriptUrl(smf_scripturl) + "action=pm");
-		};' : '', '
+		}
+		add_load_event(func);' : '', '
 	// ]]></script>
 	<title>', $context['page_title_html_safe'], '</title>';
 
