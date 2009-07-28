d037555ed79675bb2c0f46279b6b3bdbeecf6c34
thorstene <thorstene@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2009-07-28 11:01:49 +0000

    * The help icon wasn't properly aligned (GenericMenu Template) [Bug 3608]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@8830 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/GenericMenu.template.php
+++ Themes/default/GenericMenu.template.php
@@ -254,7 +254,7 @@ function template_generic_menu_tabs(&$menu_context)
 	// Show a help item?
 	if (!empty($selected_tab['help']) || !empty($tab_context['help']))
 		echo '
-			<a href="', $scripturl, '?action=helpadmin;help=', !empty($selected_tab['help']) ? $selected_tab['help'] : $tab_context['help'], '" onclick="return reqWin(this.href);" class="help"><img src="', $settings['images_url'], '/helptopics.gif" alt="', $txt['help'], '" align="top" /></a> ';
+			<a href="', $scripturl, '?action=helpadmin;help=', !empty($selected_tab['help']) ? $selected_tab['help'] : $tab_context['help'], '" onclick="return reqWin(this.href);" class="help"><img src="', $settings['images_url'], '/helptopics.gif" alt="', $txt['help'], '" class="icon" /></a> ';
 	echo '
 			', $tab_context['title'], '
 	</h3>
