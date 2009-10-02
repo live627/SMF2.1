4695d6e445605b0d9c85ab3599186f322c3c9e8d
oldiesmann <oldiesmann@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2009-10-02 16:29:23 +0000

    ! Function to create new tables wasn't working properly in PostgreSQL (DbPackages-postgresql.php) [Bug 3817]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@9150 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Sources/DbPackages-postgresql.php
+++ Sources/DbPackages-postgresql.php
@@ -183,7 +183,7 @@ function smf_db_create_table($table_name, $columns, $indexes = array(), $paramet
 			$type = $type . '(' . $size . ')';
 
 		// Now just put it together!
-		$table_query .= "\n\t\"" . $column['name'] . '" ' . $type . ' ' . (!empty($column['null']) ? '' : 'NOT NULL') . ' ' . $default;
+		$table_query .= "\n\t\"" . $column['name'] . '" ' . $type . ' ' . (!empty($column['null']) ? '' : 'NOT NULL') . ' ' . $default . ',';
 	}
 
 	// Loop through the indexes a sec...
