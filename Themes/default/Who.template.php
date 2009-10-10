5678b4b1deb6de2a09ab65d998d39a237b4c96c9
aaronvg <aaronvg@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2009-10-10 19:29:47 +0000

    * Tweak. (Who template)
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@9182 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/Who.template.php
+++ Themes/default/Who.template.php
@@ -41,7 +41,7 @@ function template_main()
 		if (!$member['is_guest'])
 		{
 			echo '
-								<span class="contact_info">
+								<span class="contact_info floatright">
 									', $context['can_send_pm'] ? '<a href="' . $member['online']['href'] . '" title="' . $member['online']['label'] . '">' : '', $settings['use_image_buttons'] ? '<img src="' . $member['online']['image_href'] . '" alt="' . $member['online']['text'] . '" align="bottom" />' : $member['online']['text'], $context['can_send_pm'] ? '</a>' : '', '
 									', $member['icq']['link'], ' ', $member['msn']['link'], ' ', $member['yim']['link'], ' ', $member['aim']['link'], '
 								</span>';
