e1ba65d2009c68f4942117c669c27aa638a87c21
aaronvg <aaronvg@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2009-10-16 21:10:50 +0000

    ! Adding a new button strip wasn't working as expected in the Core theme. (index template, theme.js)
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@9214 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/core/index.template.php
+++ Themes/core/index.template.php
@@ -135,7 +135,7 @@ function template_html_above()
 
 	echo '
 	<script type="text/javascript" src="', $settings['default_theme_url'], '/scripts/script.js?rc2"></script>
-	<script type="text/javascript" src="', $settings['default_theme_url'], '/scripts/theme.js?rc2"></script>
+	<script type="text/javascript" src="', $settings['theme_url'], '/scripts/theme.js?rc2"></script>
 	<script type="text/javascript"><!-- // --><![CDATA[
 		var smf_theme_url = "', $settings['theme_url'], '";
 		var smf_default_theme_url = "', $settings['default_theme_url'], '";
@@ -500,8 +500,8 @@ function template_button_strip($button_strip, $direction = 'top', $strip_options
 	if (!empty($buttons))
 	{
 		// Make the last one, as easy as possible.
-		$list_item = array('<li>','<li class="active">');
-		$active_item = array('<li class="last">','<li class="active last">');
+		$list_item = array('<li>', '<li class="active">');
+		$active_item = array('<li class="last">', '<li class="active last">');
 
 		$buttons[count($buttons) - 1] = str_replace($list_item, $active_item, $buttons[count($buttons) - 1]);
 	}
