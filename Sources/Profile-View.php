4269760901c588aa745077bf6c503fa5684fc7bd
antechinus <antechinus@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2009-12-04 11:51:36 +0000

    * Meh. Forgot to edit Profile-View for alternating post backgrounds didn't I. (Profile-View.php)
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@9475 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Sources/Profile-View.php
+++ Sources/Profile-View.php
@@ -485,6 +485,7 @@ function showPosts($memID)
 		$context['posts'][$counter += $reverse ? -1 : 1] = array(
 			'body' => $row['body'],
 			'counter' => $counter,
+			'alternate' => $counter % 2,
 			'category' => array(
 				'name' => $row['cname'],
 				'id' => $row['id_cat']
