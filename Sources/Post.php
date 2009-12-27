b19c0dce8a42916ba9bdc899c23dacf25d81a230
karlbenson <karlbenson@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2009-12-27 15:48:06 +0000

    ! Visual verification on qr shouldn't pass you to full post unless there was an error (Post.php) [Bug 3940]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@9571 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Sources/Post.php
+++ Sources/Post.php
@@ -1240,14 +1240,8 @@ function Post2()
 		$verificationOptions = array(
 			'id' => 'post',
 		);
-		$context['require_verification'] = create_control_verification($verificationOptions, empty($_REQUEST['from_qr']));
-		// If it's someone from quick reply, don't show them errors.
-		if (!empty($_REQUEST['from_qr']))
-		{
-				$context['post_error']['need_qr_verification'] = true;
-				return Post();
-		}
-		elseif (is_array($context['require_verification']))
+		$context['require_verification'] = create_control_verification($verificationOptions, true);
+		if (is_array($context['require_verification']))
 			$post_errors = array_merge($post_errors, $context['require_verification']);
 	}
 
