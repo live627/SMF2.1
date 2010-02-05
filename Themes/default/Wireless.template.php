95cf02c32c1c325ed54e6676f298329f6c3b22fd
thorstene <thorstene@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2010-02-05 14:46:01 +0000

    * Sending PMs in wireless mode caused an implode error (Wireless template)[Bug 3980]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@9688 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/Wireless.template.php
+++ Themes/default/Wireless.template.php
@@ -1229,7 +1229,7 @@ function template_wap2_pm()
 			echo '
 				', empty($ids) ? '' : '<input type="hidden" name="u" value="' . $ids . '" />', '<br />
 						<a href="', $scripturl, '?action=findmember', empty($ids) ? '' : ';u=' . $ids, ';', $context['session_var'], '=', $context['session_id'], ';wap2">', $txt['wireless_pm_search_member'], '</a>', empty($user_info['buddies']) ? '' : '<br />
-						<a href="' . $scripturl . '?action=pm;sa=addbuddy' . (empty($ids) ? '' : ';u=' . implode(',', $ids)) . ';wap2">' . $txt['wireless_pm_add_buddy'] . '</a>', '
+						<a href="' . $scripturl . '?action=pm;sa=addbuddy' . (empty($ids) ? '' : ';u=' . $ids) . ';wap2">' . $txt['wireless_pm_add_buddy'] . '</a>', '
 					</p>
 					<p class="windowbg">
 						<strong>', $txt['subject'], ':</strong> <input type="text" name="subject" value="', $context['subject'], '" />
