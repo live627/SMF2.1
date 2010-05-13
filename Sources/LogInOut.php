679b5ef4511973b40450be69c7843efb2cefbbb1
silentguardian <silentguardian@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2010-05-13 13:48:37 +0000

    ! When user does not have a valid id for validatePasswordFlood() try a redirect first (LogInOut.php) [Bug 2760]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@9891 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Sources/LogInOut.php
+++ Sources/LogInOut.php
@@ -689,7 +689,13 @@ function validatePasswordFlood($id_member, $password_flood_value = false, $was_c
 
 	// We need a member!
 	if (!$id_member)
+	{
+		// Redirect back!
+		redirectexit();
+
+		// Probably not needed, but still make sure...
 		fatal_lang_error('no_access', false);
+	}
 
 	// Right, have we got a flood value?
 	if ($password_flood_value !== false)
