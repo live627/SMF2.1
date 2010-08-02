30b4850beb1004785e3ba4764fec5997b50c17b7
silentguardian <silentguardian@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2010-08-02 13:26:31 +0000

    * The new replies template for preview JavaScript was using outdated post layout. (Post.template.php) [Bug 4387]  * "new" images for new replies inserted by JavaScript were not removed once the post is no longer unread. (Post.template.php) [Bug 4397]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@10046 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/Post.template.php
+++ Themes/default/Post.template.php
@@ -549,6 +549,7 @@ function template_main()
 			var make_poll = ', $context['make_poll'] ? 'true' : 'false', ';
 			var txt_preview_title = "', $txt['preview_title'], '";
 			var txt_preview_fetch = "', $txt['preview_fetch'], '";
+			var new_replies = new Array();
 			function previewPost()
 			{';
 	if ($context['browser']['is_firefox'])
@@ -661,13 +662,28 @@ function template_main()
 				if (\'num_replies\' in document.forms.postmodify.elements)
 					document.forms.postmodify.num_replies.value = XMLDoc.getElementsByTagName(\'smf\')[0].getElementsByTagName(\'num_replies\')[0].firstChild.nodeValue;
 
+				// Remove the new image from old-new replies!
+				for (i = 0; i < new_replies.length; i++)
+					document.getElementById(\'image_new_\' + new_replies[i]).style.display = \'none\';
+				new_replies = new Array();
+
 				var newPosts = XMLDoc.getElementsByTagName(\'smf\')[0].getElementsByTagName(\'new_posts\')[0] ? XMLDoc.getElementsByTagName(\'smf\')[0].getElementsByTagName(\'new_posts\')[0].getElementsByTagName(\'post\') : {length: 0};
 				var numNewPosts = newPosts.length;
 				if (numNewPosts != 0)
 				{
 					var newPostsHTML = \'<span id="new_replies"><\' + \'/span>\';
 					for (var i = 0; i < numNewPosts; i++)
-						newPostsHTML += \'<div class="title_bar"><h4 class="titlebg"><span class="floatleft">', $txt['posted_by'], ': \' + newPosts[i].getElementsByTagName("poster")[0].firstChild.nodeValue + \'</span><span class="floatright">', $txt['posted_on'], ': \' + newPosts[i].getElementsByTagName("time")[0].firstChild.nodeValue + \'<img src="\' + smf_images_url + \'/', $context['user']['language'], '/new.gif" alt="', $txt['preview_new'], '" /></span></h4></div><div class="windowbg2"><span class="topslice"><span></span></span><div class="content smalltext" id="msg\' + newPosts[i].getAttribute("id") + \'"><div class="righttext"><a href="#postmodiy" onclick="return insertQuoteFast(\\\'\' + newPosts[i].getAttribute("id") + \'\\\');">', $txt['bbc_quote'], '<\' + \'/a></div><div class="post">\' + newPosts[i].getElementsByTagName("message")[0].firstChild.nodeValue + \'<\' + \'/div></div><span class="botslice"><span></span></span></div>\';
+					{
+						new_posts[new_posts.length] = newPosts[i].getAttribute("id");
+						newPostsHTML += \'<div class="windowbg\' + (i % 2 == 0 ? \'2\' : \'\') + \' core_posts"><span class="topslice"><span></span></span><div class="content" id="msg\' + newPosts[i].getAttribute("id") + \'"><div class="floatleft"><h5>', $txt['posted_by'], ': \' + newPosts[i].getElementsByTagName("poster")[0].firstChild.nodeValue + \'</h5><span class="smalltext">&#171;&nbsp;<strong>', $txt['on'], ':</strong> \' + newPosts[i].getElementsByTagName("time")[0].firstChild.nodeValue + \'&nbsp;&#187;</span> <img src="\' + smf_images_url + \'/', $context['user']['language'], '/new.gif" alt="', $txt['preview_new'], '" id="image_new_\' + newPosts[i].getAttribute("id") + \'" /></div>\';';
+
+	if ($context['can_quote'])
+		echo '
+						newPostsHTML += \'<ul class="reset smalltext quickbuttons"><li class="quote_button"><a href="#postmodify" onclick="return insertQuoteFast(\\\'\' + newPosts[i].getAttribute("id") + \'\\\');"><span>',$txt['bbc_quote'],'</span><\' + \'/a></li></ul>\';';
+
+	echo '
+						newPostsHTML += \'<br class="clear" /><div class="list_posts smalltext">\' + newPosts[i].getElementsByTagName("message")[0].firstChild.nodeValue + \'<\' + \'/div></div><span class="botslice"><span></span></span></div>\';
+					}
 					setOuterHTML(document.getElementById(\'new_replies\'), newPostsHTML);
 				}
 
