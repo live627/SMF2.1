e123256a9b7913bac01efdde923b1220ec8e3a55
antechinus <antechinus@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2010-09-23 11:58:50 +0000

    * Minor typos and glitches (ProfileModify.php, ModerationCenter.template, Packages.template) [Bug 4436,4476]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@10129 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/Packages.template.php
+++ Themes/default/Packages.template.php
@@ -723,7 +723,7 @@ function template_browse()
 	{
 		echo '
 		<br />
-		<div class="titlet_bar">
+		<div class="title_bar">
 			<h3 class="titlebg">' . $txt['language_package'] . '</h3>
 		</div>
 		<table class="table_grid" width="100%">
@@ -902,7 +902,7 @@ function template_servers()
 	if ($context['package_download_broken'])
 	{
 		echo '
-		<div class="cat_bar">
+		<div class="title_bar">
 			<h3 class="titlebg">', $txt['package_ftp_necessary'], '</h3>
 		</div>
 		<div class="windowbg">
