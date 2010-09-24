8cf30ee841e588728caf649b628a43bd30ae9b54
norv <norv@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2010-09-24 14:38:51 +0000

    ! index.php in avatars directories was caught in a loop (avatars/*/index.php)
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@10133 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- avatars/Actors/index.php
+++ avatars/Actors/index.php
@@ -1,8 +1,8 @@
 <?php
 
 // Try to handle it with the upper level index.php.  (it should know what to do.)
-if (file_exists(dirname(__FILE__) . '/index.php'))
-	include (dirname(__FILE__) . '/index.php');
+if (file_exists(dirname(dirname(__FILE__)) . '/index.php'))
+	include (dirname(dirname(__FILE__)) . '/index.php');
 else
 	exit;
 
