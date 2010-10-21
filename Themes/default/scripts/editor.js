a3b171a72834cbe2242266da0a3f035c247f877f
norv <norv@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2010-10-21 18:12:50 +0000

    ! Fixed webkit unexpected behavior when setting font styles (thank you Nibogo!) (editor.js) [Bug 4511]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@10212 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Themes/default/scripts/editor.js
+++ Themes/default/scripts/editor.js
@@ -745,7 +745,11 @@ smc_Editor.prototype.handleSelectChange = function (oSelectProperties)
 			oSelectProperties.oSelect.selectedIndex = 0;
 		}
 		else
+		{
+			if (is_webkit)
+				this.smf_execCommand('styleWithCSS', false, true);
 			this.smf_execCommand('fontname', false, sValue);
+		}
 	}
 
 	// Font size?
