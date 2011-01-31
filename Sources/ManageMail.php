486697d7e5fc89c0d09b51f5086bb7c0d4f7b536
norv <norv@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2011-01-31 11:48:25 +0000

    ! Broken function call in ManageMail (ManageMail.php)
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@10367 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Sources/ManageMail.php
+++ Sources/ManageMail.php
@@ -134,7 +134,7 @@ function BrowseMailQueue()
 				'data' => array(
 					'function' => create_function('$rowData', '
 						global $smcFunc;
-						return $smcFunc[\'strlen\']($rowData[\'subject\']) > 50 ? sprintf(\'%1$s...\', htmlspecialchars($smcFunc[\'substr\']($rowData[\'subject\']), 0, 47)) : htmlspecialchars($rowData[\'subject\']);
+						return $smcFunc[\'strlen\']($rowData[\'subject\']) > 50 ? sprintf(\'%1$s...\', htmlspecialchars($smcFunc[\'substr\']($rowData[\'subject\'], 0, 47))) : htmlspecialchars($rowData[\'subject\']);
 					'),
 					'class' => 'smalltext',
 				),
