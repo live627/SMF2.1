8fa85ffffd4c59888f2158cb9be5720be48e5a13
silentguardian <silentguardian@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2011-04-24 13:38:09 +0000

    ! Chrome was displaying a warning when scheduled tasks were triggered via JavaScript. (ScheduledTasks.php) [Bug 4673]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@10484 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Sources/ScheduledTasks.php
+++ Sources/ScheduledTasks.php
@@ -182,7 +182,8 @@ function AutoTask()
 	// Finally, send some stuff...
 	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 	header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-	exit;
+	header('Content-Type: image/gif');
+	die("\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\x00\x00\x00\x00\x00\x00\x21\xF9\x04\x01\x00\x00\x00\x00\x2C\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3B");
 }
 
 // Function to sending out approval notices to moderators etc.
