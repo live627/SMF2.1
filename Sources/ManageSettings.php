fa39bb3ce67f14888fa43526000b9903a4979714
silentguardian <silentguardian@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2011-04-29 14:10:49 +0000

    ! integrate_modify_modifications hook should be triggered earlier in the code. (ManageSettings.php) [Bug 4638]
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@10493 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Sources/ManageSettings.php
+++ Sources/ManageSettings.php
@@ -189,6 +189,9 @@ function ModifyModSettings()
 		// Mod authors, once again, if you have a whole section to add do it AFTER this line, and keep a comma at the end.
 	);
 
+	// Make it easier for mods to add new areas.
+	call_integration_hook('integrate_modify_modifications', array(&$subActions));
+
 	loadGeneralSettingParameters($subActions, 'general');
 
 	// Load up all the tabs...
@@ -201,9 +204,6 @@ function ModifyModSettings()
 			),
 		),
 	);
-
-	// Make it easier for mods to add new areas.
-	call_integration_hook('integrate_modify_modifications', array(&$subActions, &$context[$context['admin_menu_name']]['tab_data']['tabs']));
 
 	// Call the right function for this sub-acton.
 	$subActions[$_REQUEST['sa']]();
