d670a43d61d439c4804a0a4e89fe3de24892ba50
silentguardian <silentguardian@a96bbafc-d752-43aa-a6db-eed64a5f2cf9>
2011-06-06 17:56:52 +0000

    ! More version updates.
    git-svn-id: https://devel.simplemachines.org/code/smf/trunk@10595 a96bbafc-d752-43aa-a6db-eed64a5f2cf9

--- Sources/Search.php
+++ Sources/Search.php
@@ -46,7 +46,7 @@ if (!defined('SMF'))
 // This defines two version types for checking the API's are compatible with this version of SMF.
 $GLOBALS['search_versions'] = array(
 	// This is the forum version but is repeated due to some people rewriting $forum_version.
-	'forum_version' => 'SMF 2.0 RC5',
+	'forum_version' => 'SMF 2.0',
 	// This is the minimum version of SMF that an API could have been written for to work. (strtr to stop accidentally updating version on release)
 	'search_version' => strtr('SMF 2+0=Beta=2', array('+' => '.', '=' => ' ')),
 );
