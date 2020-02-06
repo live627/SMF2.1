acb73cabb8ab6d1f2491a2720f614db0aec57cd2
John Rayes <live627@gmail.com>
2020-02-06 14:27:45 -0700

    jhyghjtyfryjhhjhf

--- tests/PMTest.php
+++ tests/PMTest.php
@@ -41,9 +41,6 @@ class PMTest extends BaseTestCase
 		);
 	}
 
-	/**
-	 * @group slowl,k
-	 */
 	public function testAddMembers()
 	{
 		global $membersTest;
@@ -93,6 +90,25 @@ class PMTest extends BaseTestCase
 	/**
 	 * @depends testSendPM
 	 */
+	public function testAddLabel() : void
+	{
+		global $context, $membersTest;
+
+		$_POST['add'] = 'send';
+		$_POST['label'] = 'test';
+		$_POST[$context['session_var']] = $context['session_id'];
+		$_POST[$_SESSION['session_var']] = $_SESSION['session_value'];
+		LoadRules(true);
+		ManageLabels();
+		unset($_POST['add']);
+
+		MessageMain();
+		$this->assertContains('test', array_column($context['labels'], 'name', 'id'));
+	}
+
+	/**
+	 * @depends testAddLabel
+	 */
 	public function testAddRule() : void
 	{
 		global $context, $membersTest;
@@ -107,28 +123,158 @@ class PMTest extends BaseTestCase
 		$_POST['rule_name'] = 'test';
 		$_POST['ruletype'] = ['mid'];
 		$_POST['ruledef'] = ['test'];
-		$_POST['acttype'] = ['del'];
+		$_POST['acttype'] = ['lab'];
+		$_POST['labdef'] = [array_keys($context['labels'])[1]];
 		$_POST[$context['session_var']] = $context['session_id'];
 		$_POST[$_SESSION['session_var']] = $_SESSION['session_value'];
 		ManageRules();
+		LoadRules(true);
+		$this->assertContains('test', array_column($context['rules'], 'name'));
+	}
+
+	/**
+	 * @depends testAddRule
+	 */
+	public function testApplyRules() : void
+	{
+		global $context, $membersTest;
 
 		$_GET['apply'] = 'send';
 		unset($_GET['save']);
 		$_GET[$context['session_var']] = $context['session_id'];
 		$_GET[$_SESSION['session_var']] = $_SESSION['session_value'];
+		ManageRules();
+
+		MessageFolder();
+		$labels = prepareMessageContext()['labels'];
+		$this->assertContains('test', array_column($labels, 'name'));
+	}
+
+	/**
+	 * @depends testAddRule
+	 */
+	public function testModifyRule() : void
+	{
+		global $context, $membersTest;
+
+		$_GET['add'] = 'send';
+		$_GET['rid'] = key($context['rules']);
 		LoadRules(true);
+		ManageRules();
+		$this->assertEquals($_GET['rid'], $context['rule']['id']);
+
+		unset($_GET['add']);
+		$_GET['save'] = 'send';
+		$_POST['rule_logic'] = 'and';
+		$_POST['rule_name'] = 'test';
+		$_POST['ruletype'] = ['mid'];
+		$_POST['ruledef'] = ['test'];
+		$_POST['acttype'] = ['del'];
+		$_POST[$context['session_var']] = $context['session_id'];
+		$_POST[$_SESSION['session_var']] = $_SESSION['session_value'];
+		ManageRules();
+		LoadRules(true);
+		$this->assertContains('test', array_column($context['rules'], 'name', 'id'));
+	}
+
+	/**
+	 * @depends testModifyRule
+	 */
+	public function testApplyModifiedRules() : void
+	{
+		global $context, $membersTest;
+
+		$_GET['apply'] = 'send';
+		unset($_GET['save']);
+		$_GET[$context['session_var']] = $context['session_id'];
+		$_GET[$_SESSION['session_var']] = $_SESSION['session_value'];
 		ManageRules();
 
 		$_REQUEST['userspec'] = 'test';
 		$_REQUEST['search'] = 'great';
 		MessageSearch2();
 		$this->assertTrue(empty($context['search_errors']));
-	//	$this->assertIsNotArray($context['personal_messages']);
 		$this->assertEmpty($context['personal_messages']);
 	}
 
 	/**
-	 * @depends testAddMemberser
+	 * @depends testAddRule
+	 */
+	public function testDeleteRule() : void
+	{
+		global $context, $membersTest;
+
+		$_GET['add'] = 'send';
+		$_GET['rid'] = key(array_column($context['rules'], 'name', 'id'));
+		LoadRules(true);
+		ManageRules();
+		$this->assertEquals($_GET['rid'], $context['rule']['id']);
+
+		unset($_GET['add']);
+		$_POST['delselected'] = 'send';
+		$_POST['delrule'] = [$_GET['rid'] => 'on'];
+		$_POST[$context['session_var']] = $context['session_id'];
+		$_POST[$_SESSION['session_var']] = $_SESSION['session_value'];
+		ManageRules();
+		unset($_POST['delselected']);
+		LoadRules(true);
+		$this->assertNotContains('test', array_column($context['rules'], 'name'));
+	}
+
+	/**
+	 * @depends testDeleteRule
+	 */
+	public function testRefreshRule() : void
+	{
+		$this->testSendPM();
+		$this->testAddRule();
+		$this->testApplyRules();
+	}
+
+	/**
+	 * @depends testAddLabel
+	 */
+	public function testSearch() : void
+	{
+		global $context, $membersTest;
+
+		MessageSearch();
+		$this->assertCount(2, $context['search_labels']);
+		$this->assertContains('test', array_column($context['search_labels'], 'name'));
+
+		$_REQUEST['userspec'] = 'test';
+		$_REQUEST['search'] = 'great';
+		MessageSearch2();
+		$this->assertTrue(empty($context['search_errors']));
+		$this->assertCount(1, $context['personal_messages']);
+		$this->assertStringContainsString('great', $context['personal_messages'][0]['body']);
+	}
+
+	/**
+	 * @depends testAddLabel
+	 */
+	public function testDeleteLabel() : void
+	{
+		global $context, $membersTest;
+
+		LoadRules(true);
+		$this->assertContains('test', array_column($context['rules'], 'name'));
+		$this->assertContains('test', array_column($context['labels'], 'name'));
+
+		$_POST['delete'] = 'send';
+		$_POST['delete_label'] = [array_keys($context['labels'])[1] => 'on'];
+		$_POST[$context['session_var']] = $context['session_id'];
+		$_POST[$_SESSION['session_var']] = $_SESSION['session_value'];
+		ManageLabels();
+		unset($_POST['delete']);
+		LoadRules(true);
+		MessageMain();
+		$this->assertNotContains('test', array_column($context['rules'], 'name'));
+		$this->assertNotContains('test', array_column($context['labels'], 'name'));
+	}
+
+	/**
+	 * @depends testAddMembers
 	 */
 	public function testRemoveMembers() : void
 	{
